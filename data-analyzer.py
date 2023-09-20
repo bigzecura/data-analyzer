d391dc30241551ab1fc83fe19d6852e570d7b948
Pratya Thanwatthanakit <43605404+l0ginp@users.noreply.github.com>
2023-09-21 00:49:45 +0700

    Update data-analyzer.py

--- data-analyzer.py
+++ data-analyzer.py
@@ -6,3 +6,12 @@ def read_csv_file(file_path):
     Read CSV file and return a Pandas DataFrame.
     """
     return pd.read_csv(file_path)
+    results = {
+        'number_of_rows': len(data),
+        'number_of_columns': len(data.columns),
+        'column_names': list(data.columns),
+        'data_types': dict(data.dtypes),
+        'missing_values': dict(data.isna().sum()),
+        'descriptive_stats': data.describe(include='all').to_dict()
+    }
+    return results
