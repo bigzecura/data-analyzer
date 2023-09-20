fb27a9c66192711f2932dd9b2cfe40ee6ebc42de
Pratya Thanwatthanakit <43605404+l0ginp@users.noreply.github.com>
2023-09-21 00:45:29 +0700

    Update data-analyzer.py

--- data-analyzer.py
+++ data-analyzer.py
@@ -6,3 +6,8 @@ def read_csv_file(file_path):
     Read CSV file and return a Pandas DataFrame.
     """
     return pd.read_csv(file_path)
+
+def basic_data_analysis(data):
+    """
+    Perform basic data analysis on the DataFrame and return the results.
+    """
